3d8ac8e625a0b1e77db53030f6968c796e7b3999
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-05-03 13:15:01 +0100

    comment updatePlan code

--- src/features/UserPlan.ts
+++ src/features/UserPlan.ts
@@ -21,6 +21,10 @@ export const userPlanSlice = createSlice({
     deletePlan: (state: { value: Plan[] }, action: PayloadAction<number>) => {
       state.value = state.value.filter((plan) => plan.id !== action.payload);
     },
+    /* mutate "state.value" by mapping through the array, for each plan, check if the ID matches the ID of the plan in the action payload.
+       If it does, it  will return a new object with the updated name and category. Otherwise, it will return the original plan object. 
+       The "state.value" array is then updated with the new array of plans.
+    */
     updatePlan: (state: { value: Plan[] }, action: PayloadAction<Plan>) => {
       state.value = state.value.map((p) => {
         if (p.id === action.payload.id) {
